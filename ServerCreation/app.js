7cb5cc911033dabbb66495595197a25c666e2772
Nickravchenko <nickravch98@gmail.com>
2022-10-20 00:12:12 -0400

    Few files

--- ServerCreation/app.js
+++ ServerCreation/app.js
@@ -1,48 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 const { parse } = require("path/posix");
 
-const server = http.createServer((req, res) => {
-  //   console.log(req.url, req.method, req.headers);
-  //   process.exit();
-  const url = req.url;
-  const method = req.method;
+const routes = require("./routes");
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</head></title>");
-    res.write("<body><h1>Klk mio, tu nombre?</h1>");
-    res.write(
-      '<form action ="message" method="POST"><input type="text" name = "message"></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("name.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        console.log(parsedBody);
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</head></title>");
-  res.write("<body><h1>Klk mio</body>");
-  res.write("</html>");
-});
-
+const server = http.createServer(routes);
 server.listen(3000);
