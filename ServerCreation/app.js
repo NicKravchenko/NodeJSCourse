738a579dacb73c641181544dd6dd189d59d3ddcc
Nickravchenko <nickravch98@gmail.com>
2022-10-19 22:15:06 -0400

    Get data from post

--- ServerCreation/app.js
+++ ServerCreation/app.js
@@ -1,9 +1,12 @@
 const http = require("http");
+const fs = require("fs");
+const { parse } = require("path/posix");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  //   console.log(req.url, req.method, req.headers);
   //   process.exit();
   const url = req.url;
+  const method = req.method;
 
   if (url === "/") {
     res.write("<html>");
@@ -16,10 +19,31 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<head><title>My First Page</head></title>");
-  res.write("<body><h1>Klk mio</body>");
-  res.write("</html>");
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFileSync("name.txt", message);
+      console.log(parsedBody);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.write("<head><title>My First Page</head></title>");
+  //   res.write("<body><h1>Klk mio</body>");
+  //   res.write("</html>");
 });
 
 server.listen(3000);
