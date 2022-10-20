7c575956fe4fa8bc8cfe481fea775fa8eca6b9ba
Nickravchenko <nickravch98@gmail.com>
2022-10-19 21:58:52 -0400

    simple routing

--- ServerCreation/app.js
+++ ServerCreation/app.js
@@ -3,11 +3,22 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   //   process.exit();
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</head></title>");
-  res.write("<body><h1>Klk mio</h1></body>");
+  const url = req.url;
 
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</head></title>");
+    res.write("<body><h1>Klk mio, tu nombre?</h1>");
+    res.write(
+      '<form action ="message" method="POST"><input type="text" name = "message"></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<head><title>My First Page</head></title>");
+  res.write("<body><h1>Klk mio</body>");
   res.write("</html>");
 });
 
