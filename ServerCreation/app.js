782684deaba9bb729959d7cea0734027e288e25b
Nickravchenko <nickravch98@gmail.com>
2022-10-20 22:39:37 -0400

    Express

--- ServerCreation/app.js
+++ ServerCreation/app.js
@@ -1,7 +1,24 @@
 const http = require("http");
 const { parse } = require("path/posix");
-
 const routes = require("./routes");
 
-const server = http.createServer(routes);
-server.listen(3000);
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Klk Main pagee</h1>");
+
+  console.log("Always runs");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In other middleware");
+  res.send("<h1>Klk prodicts</h1>");
+});
+
+// const server = http.createServer(routes);
+// const server = http.createServer(app);
+
+app.listen(3000);
