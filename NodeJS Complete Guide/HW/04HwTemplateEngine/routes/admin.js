4e06f64137b75c8956075079351a9b5df0460231
Nickravchenko <nickravch98@gmail.com>
2022-10-24 14:38:03 -0400

    Template engine HW done

--- NodeJS Complete Guide/HW/04HwTemplateEngine/routes/admin.js
+++ NodeJS Complete Guide/HW/04HwTemplateEngine/routes/admin.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const adminRouter = express.Router();
 const data = [];
+
+adminRouter.use(bodyParser.urlencoded());
 
 adminRouter.get("/", (req, res, next) => {
   res.render("car-add", { 
     pageName: "Add car",
     route: "/admin" });
 });
+
+adminRouter.post('/addCar', (req, res, next) => {
+  let carName = req.body.carName
+  console.log(carName);
+  data.push(carName);
+  res.redirect('/');
+})
 
 exports.router = adminRouter;
 exports.data = data;
--- NodeJS Complete Guide/HW/04HwTemplateEngine/routes/cars.js
+++ NodeJS Complete Guide/HW/04HwTemplateEngine/routes/cars.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
+const adminRouter = require('./admin')
+
 const carRouter = express.Router();
+const cars = adminRouter.data;
 
 carRouter.get("/", (req, res, next) => {
   res.render("car", { 
     pageName: "Car List",
-    route: "/" });
+    route: "/",
+    cars: cars});
 });
 
 module.exports = carRouter;
