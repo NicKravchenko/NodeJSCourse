30dcdfa9da71bcc7dcf1c8024735774889721ef0
Nikita Kravchenko <nickravchenko98@gmail.com>
2022-10-29 18:34:38 -0400

    Adding cart to a page

--- NodeJS Complete Guide/ServerCreation/models/product.js
+++ NodeJS Complete Guide/ServerCreation/models/product.js
@@ -2,37 +2,49 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-    __dirname,
-    '..',
-    'data', 
-    'products.json');
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(tit) {
-        this.title = tit;
-        
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save () {
-        fs.readFile(p, (err, fileContent) => {
-        console.log(fileContent);
-        let products = [];
-        if(!err) {
-            products = JSON.parse(fileContent);
-        };
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-            console.log('err');
-        })
-        });
-    }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb) {
-        fs.readFile(p, (err, fileContent) => {
-            if (err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+ 
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    })
+  }
+  
+};
